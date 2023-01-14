8ac0a90ae959cf116a46fef419494b2847626271
Zach Callaghan <zaque.callaghan@gmail.com>
2023-01-14 12:14:14 -0700

    Built overlay with multiple dialogs

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,10 +1,13 @@
-/*!
-* Start Bootstrap - Business Casual v7.0.6 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
+function on(id) {
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+    overlay.addEventListener('click',()=>off(id));
+    document.getElementById(id).showModal();
+  }
+
+function off(id) {
+    var overlay = document.getElementById("overlay");
+    overlay.removeEventListener('click',()=>off(id));
+    overlay.style.display = "none";
+    document.getElementById(id).close();
+  }
