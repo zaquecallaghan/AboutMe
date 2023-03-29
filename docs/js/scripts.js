1f12f13f278ed7b9c5c707c522d61ffb77e695dc
Zach Callaghan <zaque.callaghan@gmail.com>
2023-03-28 18:06:24 -0600

    Replaced iframes with static dialog, added photos

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,13 +1,28 @@
 function on(id) {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
-    overlay.addEventListener('click',()=>off(id));
-    document.getElementById(id).showModal();
-  }
+  var overlay = document.getElementById("overlay");
+  overlay.style.display = "block";
+  overlay.addEventListener('click', () => off(id));
+  document.getElementById(id).showModal();
+}
 
 function off(id) {
-    var overlay = document.getElementById("overlay");
-    overlay.removeEventListener('click',()=>off(id));
-    overlay.style.display = "none";
-    document.getElementById(id).close();
-  }
+  var overlay = document.getElementById("overlay");
+  overlay.removeEventListener('click', () => off(id));
+  overlay.style.display = "none";
+  document.getElementById(id).close();
+}
+
+async function show(filename) {
+  const response = await fetch(filename);
+  const content = await response.text();
+  const dialog = document.getElementById('staticdlg');
+  // set the dialog content
+  dialog.innerHTML = content;
+
+  const overlay = document.getElementById("overlay");
+  overlay.style.display = "block";
+  overlay.addEventListener('click', () => off('staticdlg'));
+
+  dialog.showModal();
+}
+
